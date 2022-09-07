5beff6f2d03eeb2d5cefdf146d57c6c161f21747
AlexYach <alexyach1973@gmail.com>
2022-09-07 12:19:13 +0300

    Використав RxJava в remote запросі

--- app/src/main/java/com/alexyach/kotlin/weatherapi/repository/remote/retrofit/IWeatherApiRetrofit.kt
+++ app/src/main/java/com/alexyach/kotlin/weatherapi/repository/remote/retrofit/IWeatherApiRetrofit.kt
@@ -2,12 +2,29 @@ package com.alexyach.kotlin.weatherapi.repository.remote.retrofit
 
 import com.alexyach.kotlin.weatherapi.repository.remote.retrofit.weatherDTO.WeatherDTO
 import com.alexyach.kotlin.weatherapi.utils.*
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface IWeatherApiRetrofit {
 
+    /** Rx */
+    @GET("/data/2.5/weather")
+    fun getWeatherByCityName(
+        @Query(WEATHER_API_KEY_NAME) keyValue: String,
+        @Query("q") nameCity: String
+    ): Observable<WeatherDTO>
+
+    @GET("/data/2.5/weather")
+    fun getWeatherByLocation(
+        @Query(WEATHER_API_KEY_NAME) keyValue: String,
+        @Query("lat") lat: Double,
+        @Query("lon") lon: Double
+    ): Observable<WeatherDTO>
+    /** --- */
+
+    /*
     @GET("/data/2.5/weather")
     fun getWeatherByCityName(
         @Query(WEATHER_API_KEY_NAME) keyValue: String,
@@ -20,4 +37,6 @@ interface IWeatherApiRetrofit {
         @Query("lat") lat: Double,
         @Query("lon") lon: Double
     ): Call<WeatherDTO>
+    */
+
 }
--- app/src/main/java/com/alexyach/kotlin/weatherapi/repository/remote/retrofit/RepositoryRetrofitImpl.kt
+++ app/src/main/java/com/alexyach/kotlin/weatherapi/repository/remote/retrofit/RepositoryRetrofitImpl.kt
@@ -1,5 +1,6 @@
 package com.alexyach.kotlin.weatherapi.repository.remote.retrofit
 
+import android.annotation.SuppressLint
 import com.alexyach.kotlin.weatherapi.repository.remote.retrofit.weatherDTO.WeatherDTO
 import com.alexyach.kotlin.weatherapi.repository.ICallbackResponse
 import com.alexyach.kotlin.weatherapi.repository.IRepositoryByLocation
@@ -8,70 +9,73 @@ import com.alexyach.kotlin.weatherapi.utils.OPENWEATHERMAP_BASE_URL
 import com.alexyach.kotlin.weatherapi.utils.WEATHER_API_KEY_VALUE
 import com.alexyach.kotlin.weatherapi.utils.converterWeatherDtoToWeatherModel
 import com.google.gson.GsonBuilder
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
 
 class RepositoryRetrofitImpl : IRepositoryByCityName, IRepositoryByLocation {
 
+    /** Rx */
+    private val retrofitImpl = Retrofit.Builder().apply {
+        baseUrl(OPENWEATHERMAP_BASE_URL)
+        addConverterFactory(
+            GsonConverterFactory.create(GsonBuilder().setLenient().create())
+        )
+        addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+    }
+
+/*
     private val retrofitImpl = Retrofit.Builder().apply {
         baseUrl(OPENWEATHERMAP_BASE_URL)
         addConverterFactory(
             GsonConverterFactory.create(GsonBuilder().setLenient().create())
         )
     }
+    */
 
     private val api: IWeatherApiRetrofit =
         retrofitImpl.build().create(IWeatherApiRetrofit::class.java)
 
+    @SuppressLint("CheckResult")
     override fun getWeatherDetailsByCityName(
         cityName: String,
         callbackResponse: ICallbackResponse
     ) {
 
-        api.getWeatherByCityName(WEATHER_API_KEY_VALUE, cityName)
-            .enqueue(object : Callback<WeatherDTO> {
+        val response: Observable<WeatherDTO> = api.getWeatherByCityName(WEATHER_API_KEY_VALUE, cityName)
+        response.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {callbackResponse.onCallbackResponse(converterWeatherDtoToWeatherModel(it))},
+                {callbackResponse.onCallbackFailure(IOException())}
+            )
 
-                override fun onResponse(
-                    call: Call<WeatherDTO>, response: Response<WeatherDTO>
-                ) {
-                    if (response.body() != null) {
-
-                        response.body()?.let {
-                            when (response.code()) {
-                                in 200..299 -> callbackResponse.onCallbackResponse(
-                                    converterWeatherDtoToWeatherModel(it)
-                                )
-
-                                429 -> callbackResponse.onCallbackFailure(IOException("Перевищений ліміт"))
-                                401 -> callbackResponse.onCallbackFailure(IOException("Щось не так з ключем API"))
-                                404 -> callbackResponse.onCallbackFailure(IOException("Не правильно введені дані"))
-                                500, 502, 503, 504 -> callbackResponse.onCallbackFailure(
-                                    IOException("Сервер іздох")
-                                )
-
-                                else -> callbackResponse.onCallbackFailure(IOException("Не відома помилка"))
-                            }
-                        }
-                    } else {
-                        callbackResponse.onCallbackFailure(IOException(response.message()))
-                    }
-                }
-
-                override fun onFailure(call: Call<WeatherDTO>, t: Throwable) {
-                    callbackResponse.onCallbackFailure(IOException("Не вдалося зв'язатися з сервером"))
-                }
-            })
     }
 
+
+    @SuppressLint("CheckResult")
     override fun getWeatherDetailsByLocation(
         lat: Double,
         lon: Double,
         callbackResponse: ICallbackResponse
     ) {
+
+        val response: Observable<WeatherDTO> = api.getWeatherByLocation(WEATHER_API_KEY_VALUE, lat, lon)
+        response.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {callbackResponse.onCallbackResponse(converterWeatherDtoToWeatherModel(it))},
+                {callbackResponse.onCallbackFailure(IOException("Не відома помилка"))}
+            )
+
+       /*
         api.getWeatherByLocation(WEATHER_API_KEY_VALUE, lat, lon)
             .enqueue(object : Callback<WeatherDTO> {
 
@@ -105,5 +109,6 @@ class RepositoryRetrofitImpl : IRepositoryByCityName, IRepositoryByLocation {
                 }
 
             })
+        */
     }
 }
